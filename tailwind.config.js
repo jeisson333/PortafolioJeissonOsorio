84db3c334540d0293da2bf0be1febbe8811472ad
jeisson333 <ljeiisonl@gmail.com>
2024-03-05 02:30:02 -0500

    se realiza configuracion de tailwind,estilos globales, Aos y fotos

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      primaryColor: "#8873ef",
+      headingColor: "#081e21",
+      smallTextColor: "#193256",
+    },
+  },
+  plugins: [],
+};
